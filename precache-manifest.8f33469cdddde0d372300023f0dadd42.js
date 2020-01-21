6984f62f749fac8cc523031b648a81eacb5f8be7
Abhishek Tadkod <41908168+abhishektadkod@users.noreply.github.com>
2020-01-21 19:06:48 +0530

    Updates

---
+++ precache-manifest.8f33469cdddde0d372300023f0dadd42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a488884436ee853b2f0083bf5d13c9",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "6d9e562f572be134ca64",
+    "url": "/resume/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/resume/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.b6fabe4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d9e562f572be134ca64",
+    "url": "/resume/static/js/main.d9bb315e.chunk.js"
+  },
+  {
+    "revision": "1d5c0405c2c30b0b6dac",
+    "url": "/resume/static/js/runtime-main.0a46d7cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
